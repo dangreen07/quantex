3c58e1da4203687e493d614dbf647f299056d9ef
Daniel Green <dangreen07@outlook.com>
2025-07-07 01:50:11 +0100

    Refactor import statements in __init__.py for clarity and consistency

--- src/quantex/__init__.py
+++ src/quantex/__init__.py
@@ -1,3 +1,14 @@
-from .sources import DataSource, BacktestingDataSource
-from .strategy import Strategy
-from .models import Bar, Tick, Order, Fill, Position, Portfolio, Trade
+from .sources import (
+    DataSource as DataSource,
+    BacktestingDataSource as BacktestingDataSource,
+)
+from .strategy import Strategy as Strategy
+from .models import (
+    Bar as Bar,
+    Tick as Tick,
+    Order as Order,
+    Fill as Fill,
+    Position as Position,
+    Portfolio as Portfolio,
+    Trade as Trade,
+)
