feat(backtester): enhance Backtester with Result class and total return calculation fix(broker): correct order execution logic and add equity calculation refactor(strategy): update Indicator class attributes for consistency test: improve test coverage for Backtester and DataSource

src/quantex/backtester.py
@@ -1,7 +1,28 @@
-import numpy as np
-
 from quantex.datasource import DataSource, PricingData
 from quantex.strategy import Indicator, Strategy
+import numpy as np
+
+
+class Result:
+    equity: np.ndarray
+    starting_cash: float
+    run_strategy: Strategy
+    total_trades: int
+
+    def __init__(
+        self,
+        equity: np.ndarray,
+        starting_cash: float,
+        run_strategy: Strategy,
+        total_trades: int = 0,
+    ):
+        self.equity = equity
+        self.starting_cash = starting_cash
+        self.run_strategy = run_strategy
+        self.total_trades = total_trades
+
+    def total_return(self) -> float:
+        return (self.equity[-1] - self.starting_cash) / self.starting_cash
 
 
 class Backtester:
@@ -17,15 +38,17 @@ class Backtester:
         self.data.add_data(data, name)
 
     def run(self):
-        for name in self.data.datas.keys():
-            self.data.datas[name]._current = 0  ## Reset the current index
         strat = self.strategy(self.data, cash=self.cash)
         strat.init()
         indicators = [
             i for i in strat.__dict__.keys() if isinstance(strat.__dict__[i], Indicator)
         ]
+        for name in self.data.datas.keys():
+            self.data.datas[name]._current = 0  ## Reset the current index
         for indicator in indicators:
-            strat.__dict__[indicator].__current__ = 0
+            strat.__dict__[indicator]._current = 0
+        initial_cash = strat.broker.cash
+        equity = np.full(len(self.data.index), initial_cash, dtype=np.float64)
         for i in range(len(self.data.index)):
             for name in self.data.datas.keys():
                 self.data.datas[
@@ -37,14 +60,14 @@ class Backtester:
             for indicator in indicators:
                 strat.__dict__[
                     indicator
-                ].__current__ += 1  ## Shouldn't have to handle multiple indexers as they should all be the same
+                ]._current += 1  ## Shouldn't have to handle multiple indexers as they should all be the same
                 ind: Indicator = strat.__dict__[indicator]
-                if ind[-1] == np.nan:
+                if np.isnan(ind[-1]):
                     skip_nan = True
             if skip_nan:
                 continue
-            print(len(strat.data.Close))
-            print(strat.__dict__[indicators[0]])
-            ## TODO: strat.next()
-            if i > 15:
-                break
+            strat.next()
+            strat.broker.__process_orders__()
+            equity[i] = strat.broker.equity()
+        self.result = Result(equity, initial_cash, strat, strat.broker.total_trades)
+        return self.result

src/quantex/strategy.py
@@ -7,17 +7,17 @@ import numpy as np
 
 class Indicator:
     def __init__(self, data: np.ndarray):
-        self.__data__ = data
-        self.__current__ = 1
+        self.__data = data
+        self._current = 1
 
     def __getitem__(self, key):
-        return self.__data__[: self.__current__][key]
+        return self.__data[: self._current][key]
 
     def __str__(self):
-        return str(self.__data__[: self.__current__])
+        return str(self.__data[: self._current])
 
     def __len__(self):
-        return self.__current__
+        return self._current
 
 
 class Strategy: